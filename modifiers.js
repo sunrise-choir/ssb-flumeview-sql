c455598cce28c967db6846e5fd5f5ebad7f40aeb
Piet Geursen <pietgeursen@gmail.com>
2019-01-17 15:40:57 +1300

    Oh yeah, sql

--- modifiers.js
+++ modifiers.js
@@ -1,37 +1,78 @@
-const messages = 'messages'
 const links = 'links'
 const keys = 'keys'
-const messageType = 'content_type'
-const isDecrypted = 'is_decrypted'
+const authors = 'authors'
 
-//Get tip of a feed
-//Get all replies to a message
+const messages = 'messages'
+const keyId = `${messages}.key_id`
+const authorId = `${messages}.author_id`
+const messageType = `${messages}.content_type`
+const isDecrypted = `${messages}.is_decrypted`
+
+// Get tip of a feed
+// Get all replies to a message
 //
 module.exports.modifiers = {
-  whereMessageType: function (query, type) {
-    query.where(
-      `${messages}.${messageType}`, type
-    )
-  },
-  whereMessageIsNotType: function (query, type) {
-    query.whereNot(
-      `${messages}.${messageType}`, type
-    )
-  },
-  whereMessageIsPrivate: function (query) {
-    query.where(
-      `${messages}.${isDecrypted}`, 1
-    )
-  },
-  whereMessageIsNotPrivate: function (query) {
-    query.whereNot(
-      `${messages}.${isDecrypted}`, 1
-    )
-  }
+  whereMessageType,
+  whereMessageIsNotType,
+  whereMessageIsPrivate,
+  whereMessageIsNotPrivate,
+  joinMessagesAuthor,
+  joinMessagesKey,
+  joinLinksFrom,
+  backLinksReferences
+
 }
 
 module.exports.strings = {
   messages,
   links,
   keys
+}
+
+function whereMessageType (query, type) {
+  query.where(
+    messageType, type
+  )
+}
+
+function whereMessageIsNotType (query, type) {
+  query.whereNot(
+    messageType, type
+  )
+}
+
+function whereMessageIsPrivate (query) {
+  query.where(
+    isDecrypted, 1
+  )
+}
+
+function whereMessageIsNotPrivate (query) {
+  query.whereNot(
+    isDecrypted, 1
+  )
+}
+
+function joinMessagesAuthor (query) {
+  query.join(authors, 'authors.id', authorId)
+}
+
+function joinMessagesKey (query) {
+  query.join(keys, 'keys.id', keyId)
+}
+
+function joinLinksFrom (query) {
+  query.join(links, 'links.link_from_id', keyId)
+}
+
+function backLinksReferences (query, id, knex) {
+  query
+    .modify(joinMessagesKey)
+    .modify(joinMessagesAuthor)
+    .modify(joinLinksFrom)
+    .where(
+      'links.link_to_id',
+      '=',
+      knex.select('keys.id').from(keys).where('keys.key', id)
+    )
 }
--- test/modifiers.js
+++ test/modifiers.js
@@ -1,18 +1,22 @@
 var test = require('tape')
 var Db = require('../')
 var rimraf = require('rimraf')
-var { messages } = require('../modifiers').strings
+var { messages, links, keys } = require('../modifiers').strings
 var {
   whereMessageType,
   whereMessageIsNotType,
   whereMessageIsPrivate,
-  whereMessageIsNotPrivate
+  whereMessageIsNotPrivate,
+  joinMessagesAuthor,
+  joinMessagesKey,
+  joinLinksFrom,
+  backLinksReferences
 } = require('../modifiers').modifiers
 
-const numRows = 1000
+const numRows = 100
 
 function createTestDB () {
-  var logPath = '/tmp/test_indexingg.sqlite'
+  var logPath = '/tmp/test_indexing.sqlite'
   var secretKey = Buffer.from('')
   rimraf.sync(logPath)
   var db = Db('/home/piet/.ssb/flume/log.offset', logPath, secretKey)
@@ -70,6 +74,74 @@ test('not private messages', function (t) {
     .modify(whereMessageIsNotPrivate)
     .then(function (result) {
       t.equal(result.length, numRows) // We don't have a secret key set, so there will be 0 private messages
+      t.end()
+      db.knex.destroy()
+    })
+})
+
+test('join author to messages', function (t) {
+  var db = createTestDB()
+
+  db
+    .knex(messages)
+    .modify(joinMessagesAuthor)
+    .limit(1)
+    .then(function (result) {
+      t.ok(result[0].author) // We don't have a secret key set, so there will be 0 private messages
+      t.end()
+      db.knex.destroy()
+    })
+})
+
+test('join message key to message', function (t) {
+  var db = createTestDB()
+  var key = '%/v5mCnV/kmnVtnF3zXtD4tbzoEQo4kRq/0d/bgxP1WI=.sha256'
+
+  db
+    .knex(messages)
+    .modify(joinMessagesKey)
+    .limit(1)
+    .then(function (result) {
+      t.equal(result[0].key, key) // We don't have a secret key set, so there will be 0 private messages
+      t.end()
+      db.knex.destroy()
+    })
+})
+
+test('join message key and authors to message', function (t) {
+  var db = createTestDB()
+  var key = '%/v5mCnV/kmnVtnF3zXtD4tbzoEQo4kRq/0d/bgxP1WI=.sha256'
+  var author = '@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519'
+
+  db
+    .knex(messages)
+    .modify(joinMessagesKey)
+    .modify(joinMessagesAuthor)
+    .limit(1)
+    .then(function (result) {
+      t.equal(result[0].key, key)
+      t.equal(result[0].author, author)
+      t.end()
+      db.knex.destroy()
+    })
+})
+
+test('backlinks', function (t) {
+  var id = '&MxwsfZoq7X6oqnEX/TWIlAqd6S+jsUA6T1hqZYdl7RM=.sha256'
+  var resultKey = '%kLWDux4wCG+OdQWAHnpBGzGlCehqMLfgLbzlKCvgesU=.sha256'
+  var db = createTestDB()
+  // give me the message.key, message.author and message.timestamp of all the messages that link_to this key.
+  db
+    .knex
+    .select([
+      'keys.key as key',
+      'authors.author as author',
+      'messages.received_time as timestamp'
+    ])
+    .from(messages)
+    .modify(backLinksReferences, id, db.knex)
+    .then(function (results) {
+      t.equal(results[0].key, resultKey)
       t.end()
       db.knex.destroy()
     })
